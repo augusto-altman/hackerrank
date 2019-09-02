3971edb165131298a73fcae3812541271f9d0279
Augusto Altman <augusto.altman@gmail.com>
2019-09-03 09:06:51 +1200

    add sock merchant

---
+++ sock-merchant/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+    const lookup = ar.reduce((acum, sockColor) => ({
+        ...acum,
+        [sockColor]: (acum[sockColor] || 0) + 1
+    }), {});
+    return Object.values(lookup)
+        .map(count => Math.floor(count/2))
+        .reduce((acum, count) => acum + count, 0);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
+
